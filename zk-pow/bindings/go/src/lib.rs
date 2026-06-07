6599201abc4d8b7c349a86e3b04458625d7f87fc
Dmytro <info@grandpool.io>
2026-06-07 22:25:41 +0000

    feat(zk-pow): add Go FFI for pool share verification and block proving

--- zk-pow/bindings/go/src/lib.rs
+++ zk-pow/bindings/go/src/lib.rs
@@ -12,11 +12,13 @@ static GLOBAL: Jemalloc = Jemalloc;
 
 mod common;
 mod mine;
+mod plain;
 mod verify;
 
 pub use common::CZKProof;
 pub use zk_pow::api::proof::{IncompleteBlockHeader, MiningConfiguration};
 
 pub use mine::mine;
+pub use plain::{mine_plain_proof, prove_and_verify_plain_proof_ffi, prove_plain_proof_ffi, verify_plain_proof_ffi};
 pub use verify::verify_zk_proof;
 pub use verify::verify_zk_proof_with_nbits;
